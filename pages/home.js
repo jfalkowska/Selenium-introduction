9f2e73b0f106b130b6585ea2a9f8f1939f2db909
jfalkowska <jfalkowska@gmail.com>
2018-06-06 18:32:24 +0200

    first commit

---
+++ pages/home.js
@@ -0,0 +1,59 @@
+const By = require("selenium-webdriver").By;
+
+class HomePage {
+  constructor(driver) {
+    this.driver = driver;
+    this.locators = {
+      inviteeForm: By.id("registrar"),
+      inviteeNameField: By.css("#registrar input[name='name']"),
+      toggleNonRespondersVisibility: By.css(".main > div input"),
+      inviteeByName: name => By.xpath(`//span[text() = "${name}"]/..`)
+
+    };
+  }
+
+  open() {
+    this.driver.get(process.env.URL);
+  }
+
+  addInvitee(name) {
+    this.driver.findElement(this.locators.inviteeNameField)
+      .sendKeys(name);
+    this.driver.findElement(this.locators.inviteeForm).submit();
+  }
+
+  toggleNonRespondersVisibility() {
+    this.driver.findElement(this.locators.toggleNonRespondersVisibility)
+      .click();
+  }
+
+  findInviteeByName(name) {
+    const el = this.driver
+      .findElement(this.locators.inviteeByName(name));
+    return new Invitee(el);
+  }
+}
+
+class Invitee {
+  constructor(element) {
+    this.element = element;
+    this.locators = {
+      removeButton: By.css("button:last-child"),
+      confirmedCheckbox: By.css("input[type='checkbox']")
+    };
+  }
+
+  remove() {
+    this.element
+      .findElement(this.locators.removeButton)
+      .click();
+  }
+
+  toggleConfirmation() {
+    this.element
+      .findElement(this.locators.confirmedCheckbox)
+      .click();
+  }
+}
+
+module.exports = HomePage;
